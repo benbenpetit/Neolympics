613bd65fa5726ded0c8fa47099f5d359525b26fa
benbenpetit <petitbenoit3@gmail.com>
2023-04-13 17:20:34 +0200

    feature/leaderboard : divider

--- src/components/modules/Game/Leaderboard/Leaderboard.vue
+++ src/components/modules/Game/Leaderboard/Leaderboard.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="c-leaderboard">
     <h2 class="c-leaderboard__subtitle">Les <span>big freak</span> des olympiques</h2>
-    <div class="c-leaderboard__scores">
+    <div class="c-leaderboard__scores o-container">
       <ul class="c-leaderboard__scores__list">
         <li v-for="maxSession in maxSessions">
           <ResultCard :user="maxSession?.user" :maxSession="maxSession.maxSession" />
         </li>
       </ul>
+      <Divider class="u-mt-double">
+        <template v-slot:main>
+          <span>Voir plus</span>
+        </template>
+      </Divider>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import Divider from '@/components/modules/Game/Leaderboard/Divider.vue'
 import ResultCard from '@/components/modules/Game/Leaderboard/Profile/ResultCard.vue'
 import { IMaxSessionWUser } from '@/core/types/IScore'
 

8ebb1452762570e366a456268afa82c8bf3a6a3e
arthurch3 <116687588+arthurch3@users.noreply.github.com>
2023-06-29 10:33:04 +0200

    minor things

---
+++ src/components/modules/Quiz/LoadingQuiz.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="--loading-quiz" id="loading-quiz">
+    <video autoplay onended="startQuiz()">
+      <source src="/video/Place_Au_Micro_Video.mp4" type="video/mp4" />
+    </video>
+  </div>
+</template>
+
+<script setup lang="ts">
+const startQuiz = () => {
+  console.log('okok')
+}
+</script>
+
+<style>
+.--loading-quiz {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 999;
+  overflow: hidden;
+}
+
+video {
+  height: 100%;
+  width: 100%;
+}
+</style>
